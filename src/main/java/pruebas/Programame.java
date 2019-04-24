1571e6a53e14f78a5e225d988d0b771c9954f54d
IreneGonz <irenegonzalezagu@gmail.com>
2019-04-24 11:05:32 +0200

    Commit prueba
    Signed-off-by: IreneGonz <irenegonzalezagu@gmail.com>

--- src/main/java/pruebas/Programame.java
+++ src/main/java/pruebas/Programame.java
@@ -8,6 +8,7 @@ public class Programame {
 	public List<String> problemaA(List<String> entrada) {
 		ArrayList salida = null;
 		return salida;
+		// Prueba
 	}
 
 	public List<String> problemaB(List<String> entrada) {
