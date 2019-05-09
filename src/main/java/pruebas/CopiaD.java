7abe5853a0f7bd33c4764f9f9dce30744e5e925a
IreneGonz <irenegonzalezagu@gmail.com>
2019-05-09 12:47:48 +0200

    Signed-off-by: IreneGonz <irenegonzalezagu@gmail.com>

---
+++ src/main/java/pruebas/CopiaD.java
@@ -0,0 +1,93 @@
+package pruebas;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CopiaD {
+	public static void main(String[] args) {
+		// No peta pero esta mal el caso de prueba (no es 12 sino 1 2)
+		problemaD2(new ArrayList<>(
+				Arrays.asList("3", "2", "1", "1 2", "2", "3", "1", "1 2", "2,3", "5", "2", "12", "3 2", "2,3")));
+
+		// Peta pero el caso de prueba esta bien
+
+		// problemaD(new ArrayList<>(
+		// Arrays.asList("3", "2", "1", "1 2", "2", "3", "1", "1 2", "2,3", "5", "2", "1
+		// 2", "3 2", "2,3")));
+	}
+
+	public static List<String> problemaD2(List<String> entrada) { // Otra prueba más
+		ArrayList salida = new ArrayList();
+		System.out.println(entrada);
+
+		if (!entrada.isEmpty()) {
+			int totalCasos = Integer.parseInt(entrada.get(0));
+			int casoActual = 0; // Lo dejo como 0 o como 1? Si lo pongo como 1 en el while sera <=
+			int datoActual = 1;
+			while (casoActual < totalCasos) {
+				int habitaciones = Integer.parseInt(entrada.get(datoActual));
+				datoActual++;
+				int conexionesEntreHabitaciones = Integer.parseInt(entrada.get(datoActual));
+				if (casoActual == 1) {
+				}
+				datoActual++;
+
+				List<String> aux = new ArrayList<String>();
+				for (int i = 0; i < conexionesEntreHabitaciones; i++) {
+					String c[] = entrada.get(datoActual).split(" ");
+					for (int j = 0; j < c.length; j++) {
+						aux.add(c[j]);
+					}
+					if (casoActual == 1) {
+						// System.out.println(entrada.get(datoActual));
+					}
+					datoActual++;
+				}
+				// Hago esto para guardar las habitaciones conectadas de esta forma:
+				// 1 2 La habitacion 1 esta conectada con la 2
+				// 3 4 La habitacion 3 esta conectada con la 4
+				int habitacionesConectadas[][] = new int[conexionesEntreHabitaciones][2];
+				for (int i = 0, x = 0; i < habitacionesConectadas.length; i++) {
+					for (int j = 0; j < habitacionesConectadas[i].length; j++) {
+						if (casoActual == 1) {
+						}
+						habitacionesConectadas[i][j] = Integer.parseInt(aux.get(x));
+						x++;
+					}
+				}
+				List<Integer> habsConec = new ArrayList<Integer>();
+				for (int i = 0; i < habitacionesConectadas.length; i++) {
+					for (int j = 0; j < habitacionesConectadas[i].length; j++) {
+						habsConec.add(habitacionesConectadas[i][j]);
+					}
+				}
+				// Aquí compruebo que se guardan bien las habitaciones conectadas en plan:
+				// 1 2 La habitacion 1 esta conectada con la 2
+				// 3 4 La habitacion 3 esta conectada con la 4
+				for (int i = 0; i < habitacionesConectadas.length; i++) {
+					for (int j = 0; j < habitacionesConectadas[i].length; j++) {
+						// System.out.println(habitacionesConectadas[i][j] + " i: " + i + " j: " + j);
+					}
+				}
+				// Los pasos pueden ir sueltos o separados por comas
+				// (En principio no hay que poner datoActual++)
+				String pAux[] = entrada.get(datoActual).split(",");
+				List<Integer> pasos = new ArrayList<Integer>();
+				for (int i = 0; i < pAux.length; i++) {
+					pasos.add(Integer.parseInt(pAux[i]));
+				}
+				datoActual++;
+
+
+				System.out.println("Habitaciones: " + habitaciones + " Num conexiones: " + conexionesEntreHabitaciones
+						+ " Habitaciones conectadas: " + habsConec + " Pasos: " + pasos);
+
+				casoActual++; // Cambiamos de caso cuando cogemos todos los datos
+			}
+		}
+
+		System.out.println(salida);
+		return salida;
+	}
+}
--- src/main/java/pruebas/Programame2.java
+++ src/main/java/pruebas/Programame2.java
@@ -7,9 +7,15 @@ import java.util.List;
 public class Programame2 {
 
 	public static void main(String[] args) {
-//		 problemaD(new ArrayList<>(
-//		 Arrays.asList("3", "2", "1", "1 2", "2", "3", "1", "1 2", "2,3", "5", "2", "1
-//		 2", "3 2", "2,3")));
+		// No peta pero esta mal el caso de prueba (no es 12 sino 1 2)
+		problemaD2(new ArrayList<>(
+				Arrays.asList("3", "2", "1", "1 2", "2", "3", "1", "1 2", "2,3", "5", "2", "1 2", "3 2", "2,3")));
+		
+		// Peta pero el caso de prueba esta bien
+
+		// problemaD(new ArrayList<>(
+		// Arrays.asList("3", "2", "1", "1 2", "2", "3", "1", "1 2", "2,3", "5", "2", "1
+		// 2", "3 2", "2,3")));
 
 		// problemaD(new ArrayList<>(
 		// Arrays.asList("2", "2", "1", "1 2", "2", "3", "1", "1 2", "2,3")));
@@ -18,104 +24,94 @@ public class Programame2 {
 		// problemaD(new ArrayList<>(Arrays.asList("1", "2", "1", "1 2", "2"))); //
 		// Tiene que dar VICTORIA
 
-		// problemaD2(new ArrayList<>(
+		// problemaD(new ArrayList<>(
 		// Arrays.asList("3", "2", "1", "1 2", "2", "3", "1", "1 2", "2,3", "5", "2", "1
 		// 2", "3 2", "2,3")));
 
-		problemaD3(new ArrayList<>(Arrays.asList("1", "2", "1", "1 2", "2")));
+		// problemaD(new ArrayList<>(Arrays.asList("1", "2", "1", "1 2", "2")));
 	}
 
-	public static List<String> problemaD(List<String> entrada) {
-		ArrayList salida = new ArrayList();
-		System.out.println(entrada);
-
-		if (!entrada.isEmpty()) {
-			// Si no esta vacia y la forma de poner los datos es correcta, aka no
-			// faltan/sobran datos
-			int casoActual = 0;
-
-			// Hay 3 casos de prueba pero hay mas de 3 entradas dentro de un caso de prueba
-			while (casoActual < Integer.parseInt(entrada.get(0))) {
-				// Recorrer lo que seria 1 caso de prueba (min son 4 entradas más pero pueden
-				// ser más)
-				// for (int i = 0; i < 4; i++) {
-				int entradaActual = 1;
-				int numHabitaciones = Integer.parseInt(entrada.get(entradaActual));
-				entradaActual++;
-				int cConexionesEntreHabitaciones = Integer.parseInt(entrada.get(entradaActual));
-				entradaActual++;
-				// Hay tantas lineas de conexiones entre habitaciones como posibilidades se
-				// hayan indicado antes
-				int conexiones[][] = new int[cConexionesEntreHabitaciones][2];
-
-				for (int k = 0; k < conexiones.length; k++) {
-					for (int j = 0; j < conexiones[k].length; j++) {
-						String aux[] = entrada.get(entradaActual).split(" ");
-						conexiones[k][0] = Integer.parseInt(aux[0]);
-						conexiones[k][1] = Integer.parseInt(aux[1]);
-						System.out.println(conexiones[k][0] + "-" + conexiones[k][1]);
-					}
-					entradaActual++;
-				}
-				// Comprobar el tema de pasos
-				// El tema de conexiones y pasos se podria hacer con una lista? en plan si
-				// conexiones.contains(pasos) o algo asi (pasos puede ir separado por , o no
-				// !!!)
-				int pasos = Integer.parseInt(entrada.get(entradaActual));
-
-				for (int i = 0; i < conexiones.length; i++) {
-					for (int j = 0; j < conexiones[i].length; j++) {
-						if (conexiones[i][j] == pasos) {
-							System.out.println("coincide");
-						}
-					}
-				}
-				// }
-				salida.add("Resultado");
-				casoActual++;
-			}
-		}
-
-		System.out.println(salida);
-		return salida;
-	}
-
-	public static List<String> problemaD2(List<String> entrada) {
-		ArrayList salida = new ArrayList();
-		System.out.println(entrada);
-
-		// Primero compruebo que la entrada no sea una lista vacia
-		if (!entrada.isEmpty()) {
-			// Todo esto se realizara x veces, tantas como casos de prueba haya
-			int casosPrueba = Integer.parseInt(entrada.get(0));
-			int numHabitaciones = Integer.parseInt(entrada.get(1));
-			String posiblesConexiones[] = entrada.get(2).split(" ");
-			// No siempre sera entrada.get(3) !!!! variara segun la longitud de
-			// posiblesConexiones!!!
-			String conexionesHabitaciones[] = entrada.get(3).split(" ");
-			String pasos[] = entrada.get(4).split(",");
-			System.out.println("Casos de prueba: " + casosPrueba);
-			System.out.println("Numero de habitaciones: " + numHabitaciones);
-			for (int i = 0; i < posiblesConexiones.length; i++) {
-				System.out.println("Conexiones: " + posiblesConexiones[i]);
-			}
-			// Lo mismo de antes, esto no funciona asi
-			System.out.println(
-					"Conexiones entre habitaciones: " + conexionesHabitaciones[0] + "-" + conexionesHabitaciones[1]);
-			System.out.println("Pasos a realizar: " + pasos[0]);
-		}
-
-		System.out.println(salida);
-		return salida;
-	}
-	
-	public static List<String> problemaD3(List<String> entrada) {
+	public static List<String> problemaD2(List<String> entrada) { // Otra prueba más
 		ArrayList salida = new ArrayList();
 		System.out.println(entrada);
 
 		if (!entrada.isEmpty()) {
 			int totalCasos = Integer.parseInt(entrada.get(0));
-			int casoActual = 0;
+			int casoActual = 0; // Lo dejo como 0 o como 1? Si lo pongo como 1 en el while sera <=
+			int datoActual = 1;
+			while (casoActual < totalCasos) {
+				int habitaciones = Integer.parseInt(entrada.get(datoActual));
+				datoActual++;
+				int conexionesEntreHabitaciones = Integer.parseInt(entrada.get(datoActual));
+				if (casoActual == 1) {
+				}
+				datoActual++;
+
+				List<String> aux = new ArrayList<String>();
+				for (int i = 0; i < conexionesEntreHabitaciones; i++) {
+					String c[] = entrada.get(datoActual).split(" ");
+					for (int j = 0; j < c.length; j++) {
+						aux.add(c[j]);
+					}
+					if (casoActual == 1) {
+						// System.out.println(entrada.get(datoActual));
+					}
+					datoActual++;
+				}
+				// Hago esto para guardar las habitaciones conectadas de esta forma:
+				// 1 2 La habitacion 1 esta conectada con la 2
+				// 3 4 La habitacion 3 esta conectada con la 4
+				int habitacionesConectadas[][] = new int[conexionesEntreHabitaciones][2];
+				for (int i = 0, x = 0; i < habitacionesConectadas.length; i++) {
+					for (int j = 0; j < habitacionesConectadas[i].length; j++) {
+						if (casoActual == 1) {
+						}
+						habitacionesConectadas[i][j] = Integer.parseInt(aux.get(x));
+						x++;
+					}
+				}
+				List<Integer> habsConec = new ArrayList<Integer>();
+				for (int i = 0; i < habitacionesConectadas.length; i++) {
+					for (int j = 0; j < habitacionesConectadas[i].length; j++) {
+						habsConec.add(habitacionesConectadas[i][j]);
+					}
+				}
+				// Aquí compruebo que se guardan bien las habitaciones conectadas en plan:
+				// 1 2 La habitacion 1 esta conectada con la 2
+				// 3 4 La habitacion 3 esta conectada con la 4
+				for (int i = 0; i < habitacionesConectadas.length; i++) {
+					for (int j = 0; j < habitacionesConectadas[i].length; j++) {
+						// System.out.println(habitacionesConectadas[i][j] + " i: " + i + " j: " + j);
+					}
+				}
+				// Los pasos pueden ir sueltos o separados por comas
+				// (En principio no hay que poner datoActual++)
+				String pAux[] = entrada.get(datoActual).split(",");
+				List<Integer> pasos = new ArrayList<Integer>();
+				for (int i = 0; i < pAux.length; i++) {
+					pasos.add(Integer.parseInt(pAux[i]));
+				}
+				datoActual++;
+
+
+				System.out.println("Habitaciones: " + habitaciones + " Num conexiones: " + conexionesEntreHabitaciones
+						+ " Habitaciones conectadas: " + habsConec + " Pasos: " + pasos);
+
+				casoActual++; // Cambiamos de caso cuando cogemos todos los datos
+			}
+		}
+
+		System.out.println(salida);
+		return salida;
+	}
+
+	public static List<String> problemaD(List<String> entrada) { // Original
+		ArrayList salida = new ArrayList();
+		System.out.println(entrada);
+
+		if (!entrada.isEmpty()) {
+			int totalCasos = Integer.parseInt(entrada.get(0));
+			int casoActual = 0; // Lo dejo como 0 o como 1? Si lo pongo como 1 en el while sera <=
 			int datoActual = 1;
 			while (casoActual < totalCasos) {
 				// Solo hay 1 linea con habitaciones
@@ -123,51 +119,93 @@ public class Programame2 {
 				datoActual++;
 				// Linea con el numero de conexiones que hay entre habitaciones
 				int conexHab = Integer.parseInt(entrada.get(datoActual));
-				// foreach recorriendo tantas lineas de conexiones entre habitaciones como
-				// conexHab haya
-				// Filas - Columnas
-				String habitacionesConectadas[] = new String[conexHab * 2];
-				
-				String conexiones2[] = new String [conexHab * 2];
+
+				//String habitacionesConectadas[] = new String[conexHab * 2];
+				// List<String> habitacionesConectadas = new ArrayList<String>();
+
+				// String conexionesString[] = new String[conexHab * 2];
+				List<String> conexionesString = new ArrayList();
+				// int conexionesInt[] = new int[conexHab * 2];
+				List<Integer> conexionesInt = new ArrayList();
 				// Es *2 porque siempre va a ser par
 				// System.out.println(conexiones2.length);
-				String[] aux;
 				datoActual++;
 				// System.out.println(entrada.get(datoActual));
-
-				for (int i = 0; i < conexiones2.length; i++) {
-
-					// Los meto aquí con el split y después si quiero lo meto en conexiones[][] más
-					// bonito
-					conexiones2 = entrada.get(datoActual).split(" ");
+				// System.out.println("Conexiones String/2: " + conexHab / 2);
+				for (int i = 0; i < conexHab; i++) {
+					String[] aux = entrada.get(datoActual).split(" ");
+					conexionesInt.add(Integer.parseInt(aux[0]));
+					conexionesInt.add(Integer.parseInt(aux[1]));
+					// System.out.println(aux[0] + "-" + aux[1]);
 					// datoActual++;
 					// Esto de arriba no estaría del todo bien, llegaría un punto en el que te
 					// quedarías sin datos
 					// Igual se podria hacer que si se puede hacer split(" ") que sume datoActual,
 					// y si no no?
-					// Esto de abajo no servirña porque si no hay espacios no se hara el split, se
+					// Esto de abajo no serviria porque si no hay espacios no se hara el split, se
 					// guardará el string tal cual
 					// if (entrada.get(datoActual).split(" ") != null)
 					// Así funciona bien (o por lo menos mejor)
-					if (entrada.get(datoActual).contains(" ")) {
+					if (entrada.get(datoActual + 1).contains(" ")) { // Y si comparo con datoActual+1?
 						datoActual++;
 					}
 				}
-				// System.out.println(entrada.get(datoActual));
+				// Ahora tocan los pasos que se realizan, que pueden ir separados por comas o
+				// ser un unico numero
+				ArrayList<Integer> pasos = new ArrayList<Integer>(); // Los metere ya como int para que sea mas facil
+				datoActual++;
+				String aux[] = entrada.get(datoActual).split(",");
+				// En el ultimo caso el 1 2 o 12 llega hasta aquí y NO DEBERIA
+				// Coge 12 como que es un paso pero es parte de las conexiones entre
+				// habitaciones, estará mal en el paso anterior
+				// System.out.println(casoActual + "-" + conexionesList.size());
 
-				// Aqui...que queria hacer?
-				// aux = entrada.get(datoActual).split(" ");
-				// for (int i = 0; i < aux.length; i++) {
-				// // System.out.println(aux[i]);
-				// }
+				// System.out.println(casoActual + "-" + conexionesList);
+				// System.out.println(datoActual + "-" + aux[0]);
 
-				// Los pasos pueden ir separados por comas o ser un unico numero
+				for (int i = 0; i < aux.length; i++) {
+					pasos.add(Integer.parseInt(aux[i]));
+				}
+				// Aquí tenemos que calcular si es VICTORIA, GAME OVER o PERDIDO (en orden
+				// contrario pero es la idea)
+				// System.out.println(pasos.size());
+				for (int i = 0; i < pasos.size(); i++) {
+					if (pasos.size() == 1) {
+						int num1 = pasos.get(0);
+						if (conexionesInt.contains(num1) && pasos.contains(habitaciones)) {
+							salida.add("VICTORIA");
+							break;
+						} else {
+							salida.add("GAME OVER");
+							break;
+						}
 
-				casoActual++; // Igual esto no hace falta? Pues si, si no peta
+					} else {
+						int num1 = pasos.get(0);
+						int num2 = pasos.get(1);
+
+						if (conexionesInt.contains(num1) && conexionesInt.contains(num2)) {
+							salida.add("VICTORIA");
+							break;
+						}
+						if (pasos.contains(habitaciones)) {
+							salida.add("GAME OVER");
+							break;
+						}
+					}
+				}
+
+				System.out.println("Habitaciones: " + habitaciones + " Num conexiones: " + conexHab
+						+ " Habitaciones conectadas: " + conexionesInt + " Pasos: " + pasos);
+
+				datoActual++;
+				casoActual++; // Cambiamos de caso cuando cogemos todos los datos
 			}
 		}
 
 		System.out.println(salida);
 		return salida;
 	}
+
+
 }
